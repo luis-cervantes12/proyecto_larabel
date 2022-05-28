a803af7d9381d84db0a05ddb8d712381cc952ac0
luis <luis.cervantes7399@alumnos.udg.mx>
2022-05-28 00:40:27 -0500

    'terminado'

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Editar Categoría</h1>
+@stop
+
+@section('content')
+
+    @if(session('info'))
+        <div class="alert alert-success">
+            <strong>{{session('info')}}</strong>
+        </div>
+    @endif
+    
+<div class="card">
+        <div class="card-body">
+            {!! Form::model($category, ['route' => ['admin.categories.update', $category], 'method' => 'put'])!!}
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Nombre de la categoría']) !!}
+                    //TODO: Validar que el nombre no sea repetido
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('slug', 'Slug') !!}
+                    {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Slug de la categoría', 'readonly']) !!}
+                </div>
+                {!! Form::submit('Actualizar', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script src="{{asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js')}}"></script>
+    <script>
+        $(document).ready( function() {
+            $("#name").stringToSlug({
+                setEvents: 'keyup keydown blur',
+                getPut: '#slug',
+                space: '-'
+            });
+        });
+    </script>
+
+@endsection
---
+++ resources/views/admin/tags/create.blade.php
@@ -0,0 +1,50 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Crear Etiqueta</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            {!! Form::open(['route' => 'admin.tags.store'])!!}
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Nombre de la etiqueta']) !!}
+                    //TODO: Validar que el nombre no sea repetido
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('slug', 'Slug') !!}
+                    {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Slug de la etiqueta', 'readonly']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('color', 'Color:') !!}
+                    {!! Form::select('color', $colors, null, ['class' => 'form-control']) !!}
+                </div>
+                {!! Form::submit('Crear', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script src="{{asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js')}}"></script>
+    <script>
+        $(document).ready( function() {
+            $("#name").stringToSlug({
+                setEvents: 'keyup keydown blur',
+                getPut: '#slug',
+                space: '-'
+            });
+        });
+    </script>
+
+@endsection
